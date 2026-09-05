039fd7f70de596bf6c2d045dba5ee698a46530d8
Sereno <isaachern7204@gmail.com>
2026-09-04 23:36:33 -0600

    Tipo de datos 04/09/2026

---
+++ src/TemasTS/01-tipos-bas.ts
@@ -0,0 +1,18 @@
+console.log("Hola Mundo");
+
+var num1 = 23;
+let num2 = 65;
+const num3 = 22;
+
+let nombre: string;
+let num: number;
+let archivo: boolean;
+let suma: number = num1 + num2 + num3;
+
+console.log("La suma es", suma);
+
+nombre = "Pedro";
+console.log(nombre);
+
+console.log(nombre);
+console.log("Prueba");
